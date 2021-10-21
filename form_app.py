17d402193e1ff996cd332423acf76aa5f98e5137
masterluoh <729624879@qq.com>
2021-10-21 12:11:47 +0800

    Initial commit

--- form_app.py
+++ form_app.py
@@ -3,15 +3,28 @@ from flask import *
 from forms import LoginForm
 
 app = Flask(__name__)
+app.app_context().push()
+app.config["SECRET_KEY"] = '2ch3D9h38SG5v43UE98hc781DGHB34'
+
 
 @app.route('/')
-def index():
-    return redirect('/basic')
+def home():
+    return "home"
 
-@app.route('/basic')
+
+@app.route('/hello')
+def index():
+    return 'Hello form'
+
+
+@app.route('/basic', methods=['GET', 'POST'])
 def basic():
     form = LoginForm()
-    return render_template('basic_form.html',form=form)
+    if form.validate_on_submit():
+        username = form.username.data
+        flash('Welcome home,%s' % username)
+        return redirect(url_for('hello'))
+    return render_template('basic_form.html', form=form)
 
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -1,14 +1,13 @@
-from wtforms import Form,StringField,PasswordField,BooleanField,SubmitField
-from wtforms.validators import DataRequired,Length
+from wtforms import Form, StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length
 from flask_wtf import FlaskForm
 from flask import *
 
-
-class LoginForm(Form):
-    username = StringField('Username',validators=[DataRequired(message='Not empty!')])
-    password = PasswordField('Password',validators=[DataRequired(),Length(8,128)])
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(message='Not empty!')])
+    password = PasswordField('Password', validators=[DataRequired(), Length(4, 128)])
     remember = BooleanField('Remember me')
     submit = SubmitField('Log in')
 
 
-form = LoginForm()
+
